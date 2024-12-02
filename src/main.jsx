65fd1b2d7aeb016323ca2ca79a9c6cd40e072d36
Kaycee34 <udechukwudestinykelechi945@gmail.com>
2024-12-02 23:04:59 +0100

    unsupported react 17 render updated

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
-import App from "./App";
-import "./index.css";
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import './index.css'
+import App from './App';
 
-ReactDOM.render(
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement);
+
+root.render(
   <BrowserRouter>
     <App />
-  </BrowserRouter>,
-  document.getElementById("root")
+  </BrowserRouter>
 );
