be157464427862cd3ff668c9aec08af176217fd6
Emeka Victor <chukwuemekav420@gmail.com>
2024-12-04 07:56:40 +0100

    created login screen for desktop

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Navbar from "../components/Navbarwrapper";
+import shopper from "../images/shopper.jpg";
 import sitelogo from "../components/site-identity/darklogo.png";
 import Footer from "../components/Footer";
 const LoginPage = () => {
@@ -7,7 +8,7 @@ const LoginPage = () => {
     <>
       {/* <Navbar /> */}
       <section
-        className="md:px-20 md:py-5 py-8 px-8 font-Poppins md:flex md:flex-col md:items-center"
+        className="flex flex-col md:px-20 md:py-5 py-8 px-8 font-Poppins md:flex md:flex-col md:items-center"
         id="Login_section"
       >
         {/* intro with logo */}
@@ -17,49 +18,56 @@ const LoginPage = () => {
           <p className="font-medium">Create account</p>
         </div>
         {/* user forms input for signup */}
-        <form className="mt-3 md:w-2/5 ">
-          {/* name */}
-          <div className="flex flex-col pt-6">
-            <label className="text-xs text-gray-600" htmlFor="">
-              Full Name
-            </label>
-            <input
-              type="text"
-              id="fullName"
-              className="px-5 py-4 bg-slate-100 focus:outline-none rounded-md mt-3"
-            />
+        <form className="mt-3 md:w-fit md:flex  ">
+          <img
+            className="hidden md:rounded-md md:flex md:w-1/2 md:h-96 md:object-cover md:items-center"
+            src={shopper}
+            alt="happy woman with shopping bag"
+          />
+          <div className="md:w-1/2 md:ml-6" id="form-div-section">
+            {/* name */}
+            <div className="flex flex-col pt-6 md:pt-0">
+              <label className="text-xs text-gray-600" htmlFor="">
+                Full Name
+              </label>
+              <input
+                type="text"
+                id="fullName"
+                className="px-5 py-4 text-xs bg-slate-100 focus:outline-none rounded-md mt-3"
+              />
+            </div>
+            {/* number or email */}
+            <div className="flex flex-col pt-4 ">
+              <label className="text-xs text-gray-600" htmlFor="">
+                Mobile Number or Email
+              </label>
+              <input
+                type="text"
+                id="numberandEmail"
+                className="px-5 py-4 text-xs bg-slate-100 focus:outline-none rounded-md mt-3"
+              />
+            </div>
+            {/* password */}
+            <div className="flex flex-col pt-4">
+              <label className="text-xs text-gray-600" htmlFor="">
+                Create a password
+              </label>
+              <input
+                type="password"
+                id="password"
+                className="px-5 py-4 text-xs bg-slate-100 focus:outline-none rounded-md mt-3"
+              />
+            </div>
+            {/* checkbox-show-password */}
+            <div className="flex pt-4">
+              <input type="checkbox" className="mr-3" />
+              <p className="text-xs text-gray-600">Show Password</p>
+            </div>
+            {/* submit button */}
+            <button className="w-full bg-orange-700 py-4 text-xs text-white rounded-md mt-4">
+              Continue
+            </button>
           </div>
-          {/* number or email */}
-          <div className="flex flex-col pt-4">
-            <label className="text-xs text-gray-600" htmlFor="">
-              Mobile Number or Email
-            </label>
-            <input
-              type="text"
-              id="numberandEmail"
-              className="px-5 py-4 bg-slate-100 focus:outline-none rounded-md mt-3"
-            />
-          </div>
-          {/* password */}
-          <div className="flex flex-col pt-4">
-            <label className="text-xs text-gray-600" htmlFor="">
-              Create a password
-            </label>
-            <input
-              type="password"
-              id="password"
-              className="px-5 py-4 bg-slate-100 focus:outline-none rounded-md mt-3"
-            />
-          </div>
-          {/* checkbox-show-password */}
-          <div className="flex pt-4">
-            <input type="checkbox" className="mr-3" />
-            <p className="text-xs text-gray-600">Show Password</p>
-          </div>
-          {/* submit button */}
-          <button className="w-full bg-orange-700 py-4 text-white rounded-md mt-4">
-            Continue
-          </button>
         </form>
         {/* already have an account */}
         <div className="mt-6 ">
